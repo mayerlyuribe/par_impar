af5ecb8c085d2fdde4a75b11332c2cf109171e3e
mayerlyuribe <mayerlyuribe@colegioguanenta.edu.co>
2023-02-17 07:45:04 -0500

    Se agrega diagrama y programa

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # par_impar
 programa para saber que números son pares y cuales no
+ 
+ # Diagrama de flujo
+
+ ![Diagrama de flujo](diagrama.png "Diagrama de flujo")
---
+++ par_impar.py
@@ -0,0 +1,12 @@
+#input
+x=int(input("digite el valor de x: "))
+
+#prossesing
+r= x%2
+if r==0:
+    msj="PAR"
+else:
+    msj="IMPAR"
+    
+#output
+print(" el numero " + " es " + msj)
